36b9759c95eed2e5bafb67f9fcd125494f775a06
Skymidz <100541047+Skymidz@users.noreply.github.com>
2022-08-24 14:50:38 +0200

    Add files via upload

---
+++ ressources/[Ajout]/[Server]/esx_status/html/scripts/app.js
@@ -0,0 +1,48 @@
+(function() {
+    let status = [];
+
+    let renderStatus = function() {
+
+        $('#status_list').html('');
+
+        for (let i = 0; i < status.length; i++) {
+
+            if (!status[i].visible) {
+                continue;
+            }
+
+            let statusDiv = $(
+                '<div class="status">' +
+                '<div class="status_inner">' +
+                '<div class="status_val"></div>' +
+                '</div>' +
+                '</div>');
+
+            $('#status_list').append(statusDiv);
+        }
+
+    };
+
+    window.onData = function(data) {
+        if (data.update) {
+            status.length = 0;
+
+            for (let i = 0; i < data.status.length; i++) {
+                status.push(data.status[i]);
+            }
+
+            renderStatus();
+        }
+
+        if (data.setDisplay) {
+            $('#status_list').css({ 'opacity': data.display });
+        }
+    };
+
+    window.onload = function(e) {
+        window.addEventListener('message', function(event) {
+            onData(event.data);
+        });
+    };
+
+})();
