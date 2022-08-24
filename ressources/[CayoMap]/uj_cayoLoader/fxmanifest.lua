6093b248e424f1be80dc611feb323b461a589297
Skymidz <100541047+Skymidz@users.noreply.github.com>
2022-08-24 17:50:44 +0200

    Add files via upload

---
+++ ressources/[CayoMap]/uj_cayoLoader/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+data_file "SCALEFORM_DLC_FILE" "stream/cpminimap/int3232302352.gfx"
+
+files {
+	"stream/cpminimap/int3232302352.gfx",
+}
+
+client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
+	'uj_cayoLoader.lua',
+}
