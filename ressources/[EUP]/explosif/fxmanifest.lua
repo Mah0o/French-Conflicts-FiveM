ac2d7ec3b0f61a16e895b56497500099f2b7e826
Skymidz <100541047+Skymidz@users.noreply.github.com>
2022-08-24 18:30:36 +0200

    Add files via upload

---
+++ ressources/[EUP]/explosif/fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'bodacious'
+games {'gta5'}
+
+author 'Rageportal'
+description 'EUP skins with newest Law & Order 8.1 and Serve & Rescue 1.3'
+version '1.0.0'
---
+++ ressources/[EUP]/m4/__resource.lua
@@ -0,0 +1,17 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+files {
+	'contentunlocks.meta',
+	'loadouts.meta',
+	'weaponcomponents.meta',
+	'weaponanimations.meta',
+	'weaponarchetypes.meta',
+	'weapons.meta'
+	}
+
+	data_file 'WEAPONINFO_FILE' 'weapons.meta'
+	data_file 'WEAPON_METADATA_FILE' 'weaponarchetypes.meta'
+	data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta'
+	data_file 'CONTENT_UNLOCKING_META_FILE' 'contentunlocks.meta'
+	data_file 'LOADOUTS_FILE' 'loadouts.meta'
+	data_file 'WEAPON_COMPONENTS' 'weaponcomponents.meta'
